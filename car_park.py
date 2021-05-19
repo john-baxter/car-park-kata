1672ef6b4a2005a0a95bb081b59bca26f7ffcb88
john-baxter <john.g.baxter@googlemail.com>
2021-05-19 15:32:08 +0100

    Add passing test for CarPark being instantiated with zero occupied spaces.

--- car_park.py
+++ car_park.py
@@ -1,3 +1,4 @@
 class CarPark():
   def __init__(self, spaces):
     self.spaces = spaces
+    self.occupied_spaces = 0
--- car_park_test.py
+++ car_park_test.py
@@ -13,7 +13,16 @@ class CarParkInitTest(unittest.TestCase):
     new_car_park = CarPark(spaces)
     expected_spaces = spaces
     actual_spaces = new_car_park.spaces
-    self.assertEqual(50, new_car_park.spaces)
+    self.assertEqual(actual_spaces, expected_spaces)
+
+  def test_when_CarPark_is_instantiated_there_are_zero_occupied_spaces(self):
+    spaces = 50
+    occupied_spaces = 0
+    new_car_park = CarPark(spaces)
+    expected_occupied_spaces = occupied_spaces
+    actual_occupied_spaces = new_car_park.occupied_spaces
+    self.assertEqual(actual_occupied_spaces, expected_occupied_spaces)
+
 
 
 if __name__ == '__main__':
